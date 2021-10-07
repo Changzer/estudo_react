647b4c918e339d5193179e81569935c3f8ff30a8
Changzer <85975702+Changzer@users.noreply.github.com>
2021-10-07 10:08:08 -0300

    testando outra vez

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          testando o ambiente.
+          testando o ambiente. mais uma vez.
         </p>
         <a
           className="App-link"
