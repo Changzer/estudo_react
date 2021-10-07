4b50db629e0f2cbc600874075316d115524a988a
Changzer <85975702+Changzer@users.noreply.github.com>
2021-10-07 16:13:32 -0300

    material ui botao

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import logo from './pokeball.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           testando o ambiente. mais uma vez.
         </p>
+
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button variant="contained" color="primary">
+            Um Botão
+          </Button>
         </a>
       </header>
     </div>
